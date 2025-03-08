25c55b557173ba157be47e9956baec87d929d0fc
Vanbersson <139586022+Vanbersson@users.noreply.github.com>
2025-03-07 22:56:53 -0300

    Correções
    Correções

--- system-concierge/src/app/services/clientecompany/clientecompany.service.ts
+++ system-concierge/src/app/services/clientecompany/clientecompany.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient, HttpHeaders, HttpResponse, HttpResponseBase } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StorageService } from '../storage/storage.service';
 import { ClientCompany } from '../../models/clientcompany/client-company';
@@ -9,10 +9,6 @@ import { ClientCompany } from '../../models/clientcompany/client-company';
   providedIn: 'root'
 })
 export class ClientecompanyService {
-
-  private urlBaseV1 = "http://10.0.0.26:8080/apiapollo/api/v1/fatclient/filter";
-
-
 
   constructor(private http: HttpClient, private storage: StorageService) { }
 
@@ -30,36 +26,30 @@ export class ClientecompanyService {
   }
 
   //Externa
-
   getIdExternal$(id: Number): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/code/" + id, { responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/code/" + id, { responseType: 'json' });
   }
-
   getFantasiaJ$(name: string): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/j/fantasia/" + name, { headers: this.myHeaders(), responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/j/fantasia/" + name, { headers: this.myHeaders(), responseType: 'json' });
   }
   getFantasiaF$(name: string): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/f/fantasia/" + name, { headers: this.myHeaders(), responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/f/fantasia/" + name, { headers: this.myHeaders(), responseType: 'json' });
   }
   getNameJ$(name: string): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/j/name/" + name, { headers: this.myHeaders(), responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/j/name/" + name, { headers: this.myHeaders(), responseType: 'json' });
   }
   getNameF$(name: string): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/f/name/" + name, { headers: this.myHeaders(), responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/f/name/" + name, { headers: this.myHeaders(), responseType: 'json' });
   }
-
   getCnpj$(cnpj: string): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/cnpj/" + cnpj, { headers: this.myHeaders(), responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/cnpj/" + cnpj, { headers: this.myHeaders(), responseType: 'json' });
   }
-
   getCpf$(cpf: string): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/cpf/" + cpf, { headers: this.myHeaders(), responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/cpf/" + cpf, { headers: this.myHeaders(), responseType: 'json' });
   }
-
   getTipo$(tipo: string): Observable<ClientCompany[]> {
-    return this.http.get<ClientCompany[]>(this.urlBaseV1 + "/tipo/" + tipo, { headers: this.myHeaders(), responseType: 'json' });
+    return this.http.get<ClientCompany[]>(environment.apiApollo + "/tipo/" + tipo, { headers: this.myHeaders(), responseType: 'json' });
   }
-
 
   private myHeaders(): HttpHeaders {
     const httpOptions = new HttpHeaders({
