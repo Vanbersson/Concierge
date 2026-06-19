66105d97da1696530a8d05518e7884f614fdccb4
Vanbersson Luiz <139586022+Vanbersson@users.noreply.github.com>
2026-06-19 13:40:01 -0300

    correções
    correções

--- system-concierge/src/app/views/crm/map/map.component.ts
+++ system-concierge/src/app/views/crm/map/map.component.ts
@@ -1,20 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [CommonModule, GoogleMap],
+  imports: [CommonModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
 export default class MapComponent {
-  center: google.maps.LatLngLiteral = {
-    lat: -8.05428,
-    lng: -34.8813
-  };
-  zoom = 12;
+
 
 
 }
